609e97b05ddf0ba31222ea2f799424c63dd29b6f
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2022-11-24 14:58:46 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <main className="bg-emerald-700 h-screen">
+      <NavBar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { FcImageFile } from "react-icons/fc";
+
+function NavBar() {
+  return (
+    <div className="bg-slate-700 px-3 py-2">
+        <div>
+            <FcImageFile className="text-6xl bg-white border-4 rounded-full"/>
+        </div>
+    </div>
+  );
+}
+
+export default NavBar;
